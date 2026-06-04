bd85be0d2f0f97f27a2b4d08ca234d2bff982532
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-04 23:37:26 +0700

    implemented 'is preload' reducer

--- src/states/isPreload/reducer.js
+++ src/states/isPreload/reducer.js
@@ -1,3 +1,13 @@
-/**
- * @TODO: Define reducer for the isPreLoad state
- */
+import { ActionType } from './action';
+
+function isPreloadReducer(isPreload = true, action = {}) {
+  const { type, payload } = action;
+  const actions = {
+    [ActionType.SET_IS_PRELOAD]: () => payload.isPreload,
+    DEFAULT: () => isPreload,
+  };
+
+  return (actions[type] || actions.DEFAULT)();
+}
+
+export default isPreloadReducer;

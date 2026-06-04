5bea594ed69c062e53f78ced2c6bb767708641c9
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-04 23:30:38 +0700

    implemented reducer for auth user

--- src/states/authUser/reducer.js
+++ src/states/authUser/reducer.js
@@ -1,3 +1,14 @@
-/**
- * @TODO: Define the reducer for the authUser state
- */
+import { ActionType } from './action';
+
+function authUserReducer(authUser = null, action = {}) {
+  const { type, payload } = action;
+  const actions = {
+    [ActionType.SET_AUTH_USER]: () => payload.authUser,
+    [ActionType.UNSET_AUTH_USER]: () => null,
+    DEFAULT: () => authUser,
+  };
+
+  return (actions[type] || actions.DEFAULT)();
+}
+
+export default authUserReducer;

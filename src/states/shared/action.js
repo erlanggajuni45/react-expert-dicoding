cbad1abea805fe8e55fd2992c4d47cc6d7ddbb87
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-05 10:37:07 +0700

    implemented thunk function for populate users and talks

--- src/states/shared/action.js
+++ src/states/shared/action.js
@@ -1,3 +1,19 @@
-/**
- * @TODO: Define all the actions (creator) that uses a combination of actions from various domain
- */
+import api from '../../utils/api';
+import { receiveUsersActionCreator } from '../users/action';
+import { receiveTalksActionCreator } from '../talks/action';
+
+function asyncPopulateUsersAndTalks() {
+  return async (dispatch) => {
+    try {
+      const users = await api.getAllUsers();
+      const talks = await api.getAllTalks();
+
+      dispatch(receiveUsersActionCreator(users));
+      dispatch(receiveTalksActionCreator(talks));
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+}
+
+export { asyncPopulateUsersAndTalks };

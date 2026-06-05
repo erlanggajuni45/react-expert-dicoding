54c89b10fbf3268cc06d4b8d1eea45ae74c7d11a
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-05 10:24:57 +0700

    implemented talk detail reducer

--- src/states/talkDetail/reducer.js
+++ src/states/talkDetail/reducer.js
@@ -1,3 +1,21 @@
-/**
- * @TODO: Define reducer for the talkDetail state
- */
+import { ActionType } from './action';
+
+function talkDetailReducer(talkDetail = null, action = {}) {
+  const { payload, type } = action;
+
+  const actions = {
+    [ActionType.RECEIVE_TALK_DETAIL]: () => payload.talkDetail,
+    [ActionType.CLEAR_TALK_DETAIL]: () => null,
+    [ActionType.TOGGLE_LIKE_TALK_DETAIL]: () => ({
+      ...talkDetail,
+      likes: talkDetail.likes.includes(payload.userId)
+        ? talkDetail.likes.filter((id) => id !== payload.userId)
+        : talkDetail.likes.concat([payload.userId]),
+    }),
+    DEFAULT: () => talkDetail,
+  };
+
+  return (actions[type] || actions.DEFAULT)();
+}
+
+export default talkDetailReducer;

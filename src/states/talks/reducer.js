30994d4ae50645d5d03a5fd0c67047fc7ea9153f
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-05 10:06:15 +0700

    implemented talks reducer

--- src/states/talks/reducer.js
+++ src/states/talks/reducer.js
@@ -1,3 +1,27 @@
-/**
- * @TODO: Define the reducer for the talks state
- */
+import { ActionType } from './action';
+
+function talksReducer(talks = [], action = {}) {
+  const { type, payload } = action;
+
+  const actions = {
+    [ActionType.RECEIVE_TALKS]: () => payload.talks,
+    [ActionType.ADD_TALK]: () => [payload.talk, ...talks],
+    [ActionType.TOGGLE_LIKE_TALK]: () =>
+      talks.map((talk) => {
+        if (talk.id === payload.talkId) {
+          return {
+            ...talk,
+            likes: talk.likes.includes(payload.userId)
+              ? talk.likes.filter((id) => id !== payload.userId)
+              : talk.likes.concat([payload.userId]),
+          };
+        }
+        return talk;
+      }),
+    DEFAULT: () => talks,
+  };
+
+  return (actions[type] || actions.DEFAULT)();
+}
+
+export default talksReducer;

378ea954024bf0f75bc11ff3da976edd22494a55
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-11 16:03:30 +0700

    created file for handle request API

---
+++ src/api/index.js
@@ -0,0 +1,290 @@
+const KEY_ACCESS_TOKEN = 'forumAccessToken';
+
+const api = (() => {
+  const BASE_URL = 'https://forum-api.dicoding.dev/v1';
+
+  async function authedRequest(url, options = {}) {
+    return fetch(url, {
+      ...options,
+      headers: {
+        ...options.headers,
+        Authorization: `Bearer ${getAccessToken()}`,
+      },
+    });
+  }
+
+  function putAccessToken(token) {
+    localStorage.setItem(KEY_ACCESS_TOKEN, token);
+  }
+
+  function getAccessToken() {
+    return localStorage.getItem(KEY_ACCESS_TOKEN);
+  }
+
+  async function register({ name, email, password }) {
+    const response = await fetch(`${BASE_URL}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ name, email, password }),
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(responseJson.message);
+    }
+
+    return data.user;
+  }
+
+  async function login({ email, password }) {
+    const response = await fetch(`${BASE_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.token;
+  }
+
+  async function getAllUsers() {
+    const response = await fetch(`${BASE_URL}/users`, {
+      method: 'GET',
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.users;
+  }
+
+  async function getOwnProfile() {
+    const response = await authedRequest(`${BASE_URL}/users/me`, {
+      method: 'GET',
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.user;
+  }
+
+  async function createThread({ title, body, category }) {
+    const response = await authedRequest(`${BASE_URL}/threads`, {
+      method: 'POST',
+      body: JSON.stringify({ title, body, category }),
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.thread;
+  }
+
+  async function getAllThreads() {
+    const response = await fetch(`${BASE_URL}/threads`, {
+      method: 'GET',
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.threads;
+  }
+
+  async function getThreadDetail(id) {
+    const response = await fetch(`${BASE_URL}/threads/${id}`, {
+      method: 'GET',
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.detailThread;
+  }
+
+  async function createComment({ threadId, content }) {
+    const response = await authedRequest(`${BASE_URL}/threads/${threadId}/comments`, {
+      method: 'POST',
+
+      body: JSON.stringify({ content }),
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.comment;
+  }
+
+  async function upvoteThread(id) {
+    const response = await authedRequest(`${BASE_URL}/threads/${id}/up-vote`, {
+      method: 'POST',
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.vote;
+  }
+
+  async function downvoteThread(id) {
+    const response = await authedRequest(`${BASE_URL}/threads/${id}/down-vote`, {
+      method: 'POST',
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.vote;
+  }
+
+  async function neutralizeThreadVote(id) {
+    const response = await authedRequest(`${BASE_URL}/threads/${id}/neutral-vote`, {
+      method: 'POST',
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.vote;
+  }
+
+  async function upvoteComment({ threadId, commentId }) {
+    const response = await authedRequest(
+      `${BASE_URL}/threads/${threadId}/comments/${commentId}/up-vote`,
+      {
+        method: 'POST',
+      },
+    );
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.vote;
+  }
+
+  async function downvoteComment({ threadId, commentId }) {
+    const response = await authedRequest(
+      `${BASE_URL}/threads/${threadId}/comments/${commentId}/down-vote`,
+      {
+        method: 'POST',
+      },
+    );
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.vote;
+  }
+
+  async function neutralizeCommentVote({ threadId, commentId }) {
+    const response = await authedRequest(
+      `${BASE_URL}/threads/${threadId}/comments/${commentId}/neutral-vote`,
+      {
+        method: 'POST',
+      },
+    );
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.vote;
+  }
+
+  async function getLeaderboards() {
+    const response = await fetch(`${BASE_URL}/leaderboards`, {
+      method: 'GET',
+    });
+
+    const responseJson = await response.json();
+    const { status, message, data } = responseJson;
+
+    if (status !== 'success') {
+      throw new Error(message);
+    }
+
+    return data.leaderboards;
+  }
+
+  return {
+    putAccessToken,
+    getAccessToken,
+    register,
+    login,
+    getAllUsers,
+    getOwnProfile,
+    createThread,
+    getAllThreads,
+    getThreadDetail,
+    createComment,
+    upvoteThread,
+    downvoteThread,
+    neutralizeThreadVote,
+    upvoteComment,
+    downvoteComment,
+    neutralizeCommentVote,
+    getLeaderboards,
+  };
+})();
+
+export default api;

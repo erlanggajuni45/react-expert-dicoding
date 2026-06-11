36f6dc915818617456973506509b180d099ff63d
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-11 14:00:09 +0700

    define needed routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import Navbar from './components/Navbar';
+import { Route, Routes } from 'react-router';
 
 function App() {
   return (
     <div className='bg-zinc-50 text-zinc-900 min-h-screen'>
       <Navbar />
-      <h1>HAI</h1>
+      <Routes>
+        <Route
+          path='/'
+          element={<h1>Home</h1>}
+        />
+        <Route
+          path='/leaderboard'
+          element={<h1>Leaderboard</h1>}
+        />
+        <Route
+          path='/login'
+          element={<h1>Login</h1>}
+        />
+        <Route
+          path='/register'
+          element={<h1>Register</h1>}
+        />
+      </Routes>
     </div>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { LogIn, MessageSquare, Trophy } from 'lucide-react';
+import { Link } from 'react-router';
 
 export default function Navbar() {
   return (
@@ -10,9 +11,11 @@ export default function Navbar() {
         </div>
         <div className='flex items-center gap-4'>
           <Trophy className='h-4 w-4' /> <span className='hidden md:inline'>Leaderboard</span>
-          <button className='flex items-center gap-2 border border-gray-200 rounded px-2 py-1 text-sm hover:bg-gray-100 transition-colors'>
-            <LogIn className='h-4 w-4' /> Login
-          </button>
+          <Link to='/login'>
+            <button className='flex items-center gap-2 border border-gray-200 rounded px-2 py-1 text-sm hover:bg-gray-100 transition-colors'>
+              <LogIn className='h-4 w-4' /> Login
+            </button>
+          </Link>
         </div>
       </div>
     </nav>

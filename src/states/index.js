e79e08945cd7b1a818f820a76e89cf93444de71e
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-12 10:25:44 +0700

    added redux states auth user

--- src/states/index.js
+++ src/states/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import uiReducer from './ui/reducer';
 import usersReducer from './users/reducer';
+import authUserReducer from './authUser/reducer';
 
 const store = configureStore({
   reducer: {
     ui: uiReducer,
     users: usersReducer,
+    authUser: authUserReducer,
   },
 });
 

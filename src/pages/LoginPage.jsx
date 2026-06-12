5f2b89934e2482d31b08f160bac5703315dbb2ff
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-12 10:30:46 +0700

    bind input controller on login page

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import { Link } from 'react-router';
+import useInput from '../hooks/useInput';
 
 export default function LoginPage() {
+  const [email, onEmailChange] = useInput('');
+  const [password, onPasswordChange] = useInput('');
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(email, password);
+  };
+
   return (
     <div className='flex flex-col items-center justify-center gap-4 h-screen'>
       <div className='bg-white rounded-lg shadow-md p-8 w-full max-w-md'>
         <h1 className='font-semibold text-2xl'>Selamat datang kembali</h1>
         <p className='text-gray-500 text-sm py-1'>Masuk untuk ikut berdiskusi</p>
 
-        <form className='flex flex-col gap-2 mt-4'>
+        <form
+          className='flex flex-col gap-2 mt-4'
+          onSubmit={onSubmit}
+        >
           <label htmlFor='email'>Email</label>
           <input
             type='email'
             id='email'
             className='border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500'
+            value={email}
+            onChange={onEmailChange}
           />
           <label htmlFor='password'>Password</label>
           <input
             type='password'
             id='password'
             className='border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500'
+            value={password}
+            onChange={onPasswordChange}
           />
           <button
             type='submit'

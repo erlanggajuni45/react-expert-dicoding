d90d0f3b3f8d154ac545a6f237b9eaca7e517784
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-12 16:03:11 +0700

    show thread content from props

--- src/components/ThreadCard.jsx
+++ src/components/ThreadCard.jsx
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 
 import { formatDistanceToNow } from 'date-fns';
+import { MessageCircle } from 'lucide-react';
 import React from 'react';
 import z from 'zod';
 
@@ -32,7 +33,7 @@ export default function ThreadCard({ thread, owner }) {
   }
 
   return (
-    <div className='bg-white rounded-md p-4 shadow-sm'>
+    <div className='bg-white rounded-xl p-4 my-1 shadow-sm'>
       <header className='flex items-center gap-2'>
         <img
           src={owner.avatar}
@@ -41,13 +42,27 @@ export default function ThreadCard({ thread, owner }) {
         />
         <span>
           {owner.name} <span>·</span>{' '}
-          <span className='text-black/50'>{formatDistanceToNow(new Date(thread.createdAt))}</span>
+          <span className='text-black/50'>
+            {formatDistanceToNow(new Date(thread.createdAt), { addSuffix: true })}
+          </span>
         </span>
         <div className='bg-slate-200 px-2 py-1 rounded-md ml-auto text-xs font-semibold'>
           #{thread.category}
         </div>
       </header>
-      <div className='line-clamp-2'></div>
+      <div className='mt-3'>
+        <h3 className='text-xl font-semibold'>{thread.title}</h3>
+        <div
+          className='mt-3 line-clamp-2 text-zinc-500'
+          dangerouslySetInnerHTML={{ __html: thread.body }}
+        />
+      </div>
+      <div className='mt-3 flex pr-2'>
+        <span className='ml-auto flex items-center gap-2'>
+          <MessageCircle className='h-4 w-4' />
+          {thread.totalComments}
+        </span>
+      </div>
     </div>
   );
 }

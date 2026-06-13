a06dca0f987a5cc218a054380897f9dfb9166c3f
Erlangga Juni Saputra <erlanggajunisaputra@gmail.com>
2026-06-14 00:48:48 +0700

    implemented vote function on thread detail component

--- src/pages/DetailThreadPage.jsx
+++ src/pages/DetailThreadPage.jsx
@@ -1,14 +1,16 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router';
-import { asyncGetThreadDetail } from '../states/threadDetail/action';
+import { asyncApplyVoteThreadDetail, asyncGetThreadDetail } from '../states/threadDetail/action';
 import Skeleton from 'react-loading-skeleton';
 import { formatDistanceToNow } from 'date-fns';
 import CommentCard from '../components/CommentCard';
 import CommentPost from '../components/CommentPost';
+import VoteButtons from '../components/VoteButtons';
 
 export default function DetailThreadPage() {
   const threadDetail = useSelector((state) => state.threadDetail);
+  const authUser = useSelector((state) => state.authUser);
 
   const { threadId } = useParams();
 
@@ -31,6 +33,31 @@ export default function DetailThreadPage() {
   const { title, body, createdAt, owner, category, comments, upVotesBy, downVotesBy } =
     threadDetail;
 
+  const upActive = !!authUser && upVotesBy.includes(authUser.id);
+  const downActive = !!authUser && downVotesBy.includes(authUser.id);
+
+  const handleVote = async (type) => {
+    if (!authUser) {
+      alert('Anda harus login terlebih dahulu!');
+      navigate('/login');
+      return;
+    }
+
+    let voteType = type;
+    if (type === 'up' && upActive) {
+      voteType = 'neutral';
+    }
+    if (type === 'down' && downActive) {
+      voteType = 'neutral';
+    }
+
+    try {
+      await dispatch(asyncApplyVoteThreadDetail({ threadId, voteType }));
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
   return (
     <div className='px-4 py-8 mx-auto max-w-3xl'>
       <div className='mb-6 bg-white rounded-lg border border-slate-300 p-4 flex flex-col gap-3'>
@@ -52,6 +79,14 @@ export default function DetailThreadPage() {
           </div>
         </div>
         <div dangerouslySetInnerHTML={{ __html: body }} />
+        <VoteButtons
+          upCount={upVotesBy.length}
+          downCount={downVotesBy.length}
+          upActive={upActive}
+          downActive={downActive}
+          onUp={() => handleVote('up')}
+          onDown={() => handleVote('down')}
+        />
       </div>
       <CommentPost
         commentCount={comments.length}

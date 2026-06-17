6b3fc2e1ff4435f17a379038a24d4e5ef9f2bf44
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2026-06-17 11:45:11 +0700

    remove unused action on ui reducer

--- src/states/ui/action.js
+++ src/states/ui/action.js
@@ -1,19 +1,9 @@
 const ActionType = {
   START_LOADING: 'START_LOADING',
   FINISH_LOADING: 'FINISH_LOADING',
-  SET_LOADING: 'SET_LOADING',
 };
 
 const startLoadingActionCreator = () => ({ type: ActionType.START_LOADING });
 const finishLoadingActionCreator = () => ({ type: ActionType.FINISH_LOADING });
-const setLoadingActionCreator = (isLoading) => ({
-  type: ActionType.SET_LOADING,
-  payload: isLoading,
-});
 
-export {
-  ActionType,
-  startLoadingActionCreator,
-  finishLoadingActionCreator,
-  setLoadingActionCreator,
-};
+export { ActionType, startLoadingActionCreator, finishLoadingActionCreator };

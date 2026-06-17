8eabd0d637cb6ff97790d65d4ebf93fbb7770e87
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2026-06-17 22:01:42 +0700

    added test file for thread detail thunks

---
+++ src/states/threadDetail/action.test.js
@@ -0,0 +1,9 @@
+import { describe } from 'vitest';
+
+describe('asyncGetThreadDetail thunk', () => {});
+
+describe('asyncAddCommentThread thunk', () => {});
+
+describe('asyncApplyVoteThreadDetail thunk', () => {});
+
+describe('asyncApplyVoteComment thunk', () => {});

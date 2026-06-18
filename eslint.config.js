dd0284b386d56742d7aa882d00ba4b29d0917f5d
Erlangga Juni Saputra <33561813+erlanggajuni45@users.noreply.github.com>
2026-06-19 02:14:28 +0700

    setup storybook

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import js from '@eslint/js';
 import globals from 'globals';
 import pluginReact from 'eslint-plugin-react';
@@ -5,24 +8,18 @@ import { defineConfig } from 'eslint/config';
 import pluginCypress from 'eslint-plugin-cypress/flat';
 import daStyle from 'eslint-config-dicodingacademy';
 
-export default defineConfig([
-  {
-    files: ['**/*.{js,mjs,cjs,jsx}'],
-    plugins: { js },
-    extends: ['js/recommended'],
-    languageOptions: { globals: { ...globals.browser, ...globals.node } },
-    settings: {
-      react: {
-        version: 'detect',
-      },
+export default defineConfig([{
+  files: ['**/*.{js,mjs,cjs,jsx}'],
+  plugins: { js },
+  extends: ['js/recommended'],
+  languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  settings: {
+    react: {
+      version: 'detect',
     },
   },
-  pluginReact.configs.flat.recommended,
-  pluginCypress.configs.recommended,
-  daStyle,
-  {
-    rules: {
-      'react/prop-types': 'off',
-    },
+}, pluginReact.configs.flat.recommended, pluginCypress.configs.recommended, daStyle, {
+  rules: {
+    'react/prop-types': 'off',
   },
-]);
+}, ...storybook.configs["flat/recommended"]]);
